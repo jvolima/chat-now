7097378ade31459051f091db95356fca4300f67a
jvolima <jvolima2004@gmail.com>
2022-04-01 10:47:40 -0300

    Iniciando componenten Header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+    </Container>
+  )
+}
---
+++ frontend/src/styles/signIn.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  max-width: 1120px;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    font-size: 2.5rem;
+  }
+`
